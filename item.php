87ff2cb9ec2ccc793ffbb9150f0693240b6b6629
Nextor <nextor@gmail.com>
2021-05-06 11:25:11 -0700

    Normalizacion

--- item.php
+++ item.php
@@ -12,6 +12,7 @@ use \akou\ArrayUtils;
 use \akou\ValidationException;
 use \akou\LoggableException;
 use \akou\SystemException;
+use \akou\SessionException;
 
 
 class Service extends SuperRest
@@ -21,6 +22,14 @@ class Service extends SuperRest
 		session_start();
 		App::connect();
 		$this->setAllowHeader();
+
+
+		$user = app::getUserFromSession();
+
+		if( !$user )
+			return $this->sendStatus( 401 )->json(array("error"=>'Por favor iniciar sesion'));
+
+
 		$this->is_debug = false;
 		$extra_join = '';
 		$extra_sort = array();
@@ -33,78 +42,6 @@ class Service extends SuperRest
 
 
 		return $this->genericGet("item",[],$extra_join,$extra_sort);
-	}
-
-	function post()
-	{
-		$this->setAllowHeader();
-		$params = $this->getMethodParams();
-		app::connect();
-		DBTable::autocommit(false );
-
-		try
-		{
-			$user = app::getUserFromSession();
-			if( $user == null )
-				throw new ValidationException('Please login');
-
-			$is_assoc	= $this->isAssociativeArray( $params );
-			$result		= $this->batchInsert( $is_assoc  ? array($params) : $params );
-			DBTable::commit();
-			return $this->sendStatus( 200 )->json( $is_assoc ? $result[0] : $result );
-		}
-		catch(LoggableException $e)
-		{
-			DBTable::rollback();
-			return $this->sendStatus( $e->code )->json(array("error"=>$e->getMessage()));
-		}
-		catch(\Exception $e)
-		{
-			DBTable::rollback();
-			return $this->sendStatus( 500 )->json(array("error"=>$e->getMessage()));
-		}
-	}
-
-	function put()
-	{
-		$this->setAllowHeader();
-		$params = $this->getMethodParams();
-		app::connect();
-		DBTable::autocommit( false );
-
-		try
-		{
-			$user = app::getUserFromSession();
-			if( $user == null )
-				throw new ValidationException('Please login');
-
-			$is_assoc	= $this->isAssociativeArray( $params );
-			$result		= $this->batchUpdate( $is_assoc  ? array($params) : $params );
-			DBTable::commit();
-			return $this->sendStatus( 200 )->json( $is_assoc ? $result[0] : $result );
-		}
-		catch(LoggableException $e)
-		{
-			DBTable::rollback();
-			return $this->sendStatus( $e->code )->json(array("error"=>$e->getMessage()));
-		}
-		catch(\Exception $e)
-		{
-			DBTable::rollback();
-			return $this->sendStatus( 500 )->json(array("error"=>$e->getMessage()));
-		}
-
-	}
-
-	function batchInsert($array)
-	{
-		return $this->genericInsert($array,"item");
-	}
-
-	function batchUpdate($array)
-	{
-		$insert_with_ids = false;
-		return $this->genericUpdate($array, "item", $insert_with_ids );
 	}
 
 	/*
