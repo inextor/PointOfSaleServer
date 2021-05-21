2e3c26da69ed57e09990d8b3f2ba44db894a4520
Nextor <nextor@gmail.com>
2021-05-21 14:30:31 -0700

    Agregando Funds

---
+++ fund.php
@@ -0,0 +1,72 @@
+<?php
+namespace APP;
+
+include_once( __DIR__.'/app.php' );
+include_once( __DIR__.'/akou/src/ArrayUtils.php');
+include_once( __DIR__.'/SuperRest.php');
+
+use \akou\Utils;
+use \akou\DBTable;
+use \akou\RestController;
+use \akou\ArrayUtils;
+use \akou\ValidationException;
+use \akou\LoggableException;
+use \akou\SystemException;
+use \akou\SessionException;
+
+
+class Service extends SuperRest
+{
+	function get()
+	{
+		session_start();
+		App::connect();
+		$this->setAllowHeader();
+
+		$extra_constraints=array();
+		$extra_joins = '';
+		$extra_sort = array();
+		$this->is_debug = true;
+		return $this->genericGet("fund",$extra_constraints,$extra_joins,$extra_sort);
+	}
+
+	function post()
+	{
+		$this->setAllowHeader();
+		$params = $this->getMethodParams();
+		app::connect();
+		DBTable::autocommit(false );
+
+		try
+		{
+			$user = app::getUserFromSession();
+			if( $user == null )
+				throw new ValidationException('Please login');
+
+			$is_assoc	= $this->isAssociativeArray( $params );
+			$result		= $this->batchInsert( $is_assoc  ? array($params) : $params );
+			DBTable::commit();
+			return $this->sendStatus( 200 )->json( $is_assoc ? $result[0] : $result );
+		}
+		catch(LoggableException $e)
+		{
+			DBTable::rollback();
+			return $this->sendStatus( $e->code )->json(array("error"=>$e->getMessage()));
+		}
+		catch(\Exception $e)
+		{
+			DBTable::rollback();
+			return $this->sendStatus( 500 )->json(array("error"=>$e->getMessage()));
+		}
+	}
+
+	function batchInsert($array)
+	{
+		$user				= app::getUserFromSession();
+		$optional_values	= null;
+		$system_values		= array('created_by_user_id'=>$user->id,'updated_by_user_id'=>$user->id);
+		return $this->genericInsert($array,"fund",$optional_values,$system_values);
+	}
+}
+$l = new Service();
+$l->execute();
